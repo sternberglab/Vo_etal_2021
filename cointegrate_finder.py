fea1ea2bfd8b2b2883369d65d382f8d9c08d988a
Chris Acree <acreechristopher@gmail.com>
2020-08-07 22:29:16 -0400

    With output name

--- cointegrate_finder.py
+++ cointegrate_finder.py
@@ -180,7 +180,7 @@ def get_read_obj(hit, tn_read, reads_seqrecs):
 		prev_end = hsp.hit_end
 	return read_result
 
-def do_blast(query_file, subject_file,):
+def do_blast(query_file, subject_file, output_name):
 	cline = NcbiblastnCommandline(query=query_file, subject=subject_file, num_alignments=10000, out=Path(f'./{output_name}'), outfmt=5)
 	subprocess.run(str(cline), shell=True)
 	return output_name
